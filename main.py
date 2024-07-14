eb092619e48eb5bbb28d5b06b34c581ea6d33c54
MD <dorianlaughor@gmail.com>
2024-07-14 23:50:59 +0500

    make draft py file add req file and change some ignore file

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pymongo.mongo_client import MongoClient
+import os
+from dotenv import load_dotenv
+
+app = FastAPI()
+
+load_dotenv()
+
+uri = os.getenv("MONGODB_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
